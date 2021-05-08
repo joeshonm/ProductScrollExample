ac293e8cfc3b857d742cc338a01cbb41045f56a0
JoeShon Monroe <me@joeshonm.com>
2021-05-08 14:44:34 -0400

    Initial Commit

---
+++ ProductScrollExample/ProductScrollExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductScrollExampleApp.swift
+//  ProductScrollExample
+//
+//  Created by JoeShon Monroe on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ProductScrollExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
